6a8c73572cb5143fee162602f2052f5eddfb365a
Nathan <285184704+nathan38100@users.noreply.github.com>
2026-05-21 11:34:01 +0200

    update 2026-05-21 11:34:01

---
+++ Install.bat
@@ -0,0 +1,63 @@
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+set "DEST=C:\Program Files\LRPhoton"
+
+echo.
+echo =====================================
+echo        Installation de LRPhoton
+echo =====================================
+echo.
+
+echo Creation du dossier...
+mkdir "%DEST%" 2>nul
+
+echo.
+echo Copie des fichiers...
+xcopy "%CD%" "%DEST%" /E /I /Y
+
+cd /d "%DEST%"
+
+echo.
+echo Installation des dependances Python...
+py -m pip install --upgrade pip
+py -m pip install PySide6 numpy matplotlib h5py fabio requests hdf5plugin
+
+echo.
+echo Creation du lanceur LRPhoton.bat...
+
+echo @echo off > "%DEST%\LRPhoton.bat"
+echo cd /d "C:\Program Files\LRPhoton" >> "%DEST%\LRPhoton.bat"
+echo start "" pyw "main.py" >> "%DEST%\LRPhoton.bat"
+echo exit >> "%DEST%\LRPhoton.bat"
+
+echo.
+echo Creation du raccourci bureau...
+
+powershell -Command ^
+"$WshShell = New-Object -ComObject WScript.Shell; ^
+$Shortcut = $WshShell.CreateShortcut('%USERPROFILE%\Desktop\LRPhoton.lnk'); ^
+$Shortcut.TargetPath = 'C:\Program Files\LRPhoton\LRPhoton.bat'; ^
+$Shortcut.WorkingDirectory = 'C:\Program Files\LRPhoton'; ^
+$Shortcut.IconLocation = 'C:\Program Files\LRPhoton\assets\LRPhoton.ico'; ^
+$Shortcut.Save()"
+
+echo.
+echo Lancement de LRPhoton...
+start "" pyw "main.py"
+
+echo.
+echo =====================================
+echo     Installation terminee
+echo =====================================
+echo.
+echo Programme installe dans :
+echo C:\Program Files\LRPhoton
+echo.
+echo Un raccourci LRPhoton a ete cree
+echo sur le bureau.
+echo.
+
+pause
